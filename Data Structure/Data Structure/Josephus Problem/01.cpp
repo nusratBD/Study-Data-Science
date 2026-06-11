df01e25ee17b407ae08ec688962d285c75c2e1af
nusratBD <nusrat.hurain15@gmail.com>
2026-06-11 21:33:18 +0600

    Josephus Algorithm

--- Data Structure/Data Structure/Josephus Problem/01.cpp
+++ Data Structure/Data Structure/Josephus Problem/01.cpp
@@ -1,19 +1,59 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
-
-int josephusMath(int n, int k) {
-    int survivor = 0; // বেস কেস: ১ জন মানুষের জন্য ০ নম্বর ব্যক্তি বিজয়ী
-
-    // ২ জন মানুষ থেকে শুরু করে n জন মানুষ পর্যন্ত লুপ চলবে
-    for (int i = 2; i <= n; i++) {
-        survivor = (survivor + k) % i; // সেই ম্যাজিক সূত্র!
-    }
-
-    return survivor + 1; // 1-indexed করার জন্য শেষে ১ যোগ করা হলো
+void display();
+struct Node{
+int data;
+Node *next;
+};
+Node *head=NULL;
+Node *tail=NULL;
+void insert(int n){
+if(head==NULL){
+    Node *newNode=new Node();
+    newNode->data=1;
+    head=newNode;
+    tail=newNode;
+    newNode->next=head;
 }
-
-int main() {
-    int n = 10, k = 2;
-    cout << "Last survivor " << josephusMath(n, k) << endl;
-    return 0;
+    for(int i=2; i<=n; i++){
+        Node *newNode=new Node();
+        newNode->data=i;
+        newNode->next=head;
+        tail->next=newNode;
+        tail=newNode;
+    }
+    display();
+}
+void display(){
+Node *temp=head;
+if(head!=NULL){
+    while(temp->next!=head){
+        cout<<temp->data<<" ";
+        temp=temp->next;
+    }
+    cout<<temp->data<<endl;
+}
+else{
+    cout<<"List Empty."<<endl;
+}
+}
+void josephus_problem(int n, int k){
+insert(n);
+Node *temp=head;
+Node *x;
+Node *y;
+while(temp!=temp->next){
+ for(int i=1; i<k; i++){
+    temp=temp->next;
+ }
+ x=temp;//target node er ager node
+ y=temp->next;//target node;
+ temp=y->next;//target node er porer node
+ x->next=temp;
+ delete y;
+}
+cout<<temp->data<<endl;
+}
+int main(){
+    josephus_problem(10,3);
 }
